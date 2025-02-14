76462c9bb907dad7e38526ef52473e79224297ef
ravvi-kumar <ravvi.kumar8989@gmail.com>
2025-02-14 16:47:03 +0530

    hono api with presigned url

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,96 @@
 import { Hono } from 'hono'
+import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 const app = new Hono()
 
+// Initialize S3 client
+const s3Client = new S3Client({
+  endpoint: 'http://localhost:4566',
+  forcePathStyle: true,
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: 'test',
+    secretAccessKey: 'test'
+  }
+});
+
+// Constants
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+const BUCKET_NAME = 'my-local-bucket';
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.post('/generate-upload-url', async (c) => {
+  try {
+
+    const body = await c.req.json()
+    const fileName = body.fileName;
+    const fileType = body.fileType;
+    const fileSize = body.fileSize;
+
+
+    // #NOTE : from file
+    // const formData = await c.req.formData()
+    // const file = formData.get('file') as File
+    // const fileName = file.name;
+    // const fileType = file.type;
+    // const fileSize = file.size;
+
+    // // Validate if file exists
+    // if (!file) {
+    //   return c.json({ error: 'No file provided' }, 400)
+    // }
+
+    if (!fileName || !fileType || !fileSize) {
+      return c.json({ error: 'fileName, fileSize and fileType are required' }, 400)
+    }
+
+    // Validate file size
+    if (fileSize > MAX_FILE_SIZE) {
+      return c.json({ error: `File size exceeds ${MAX_FILE_SIZE}MB limit` }, 400)
+    }
+
+    const fileKey = `${fileName}-${Math.random().toString(36).substring(2)}`;
+
+    const command = new PutObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: fileKey,
+      ContentType: fileType,
+      ContentLength: fileSize,
+      // ACL: 'public-read',
+      // ACL: 'private',
+      Metadata: {
+        userId: '12345'
+      }
+    })
+
+    const presignedUrl = await getSignedUrl(s3Client, command, { expiresIn: 60 });
+
+    return c.json({
+      uploadUrl: presignedUrl,
+      fileName: fileName,
+      expiresIn: 60
+    })
+
+  } catch (error) {
+    console.error('Error generating presigned URL:', error)
+    return c.json({ error: 'Failed to generate upload URL' }, 500)
+  }
+})
+
+app.get("/file/:fileKey", async (c) => {
+  const fileKey = c.req.param("fileKey")
+  const command = new GetObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: fileKey
+  })
+  const url = await getSignedUrl(s3Client, command, { expiresIn: 60 });
+  return c.json({
+    url: url
+  })
 })
 
 export default app
--- src/test.ts
+++ src/test.ts
@@ -2,35 +2,40 @@ import { S3Client, CreateBucketCommand, PutObjectCommand, ListObjectsCommand } f
 import * as fs from 'fs';
 
 const s3Client = new S3Client({
-  endpoint: 'http://localhost:4566',
-//   endpoint: 'http://s3.localhost.localstack.cloud:4566',
-  forcePathStyle: true,
-  region: 'us-east-1',
-  credentials: {
-    accessKeyId: 'test',
-    secretAccessKey: 'test'
-  }
+    endpoint: 'http://localhost:4566',
+    //   endpoint: 'http://s3.localhost.localstack.cloud:4566',
+    forcePathStyle: true,
+    region: 'us-east-1',
+    credentials: {
+        accessKeyId: 'test',
+        secretAccessKey: 'test'
+    }
 });
 
+async function createBucket(bucketName: string) {
+    const bucketCreationResponse = await s3Client.send(new CreateBucketCommand({ Bucket: bucketName }));
+    console.log("bucket created:", bucketCreationResponse)
+    return bucketCreationResponse;
+}
+
 async function runS3Test() {
-  const bucketName = 'my-local-bucket';
+    const bucketName = 'my-local-bucket';
 
-  // Create bucket
-  const bucketCreationResponse = await s3Client.send(new CreateBucketCommand({ Bucket: bucketName }));
-  console.log("bucket created:", bucketCreationResponse)
+    // Create bucket
+    await createBucket(bucketName);
 
-  // Upload file
-  const fileContent = fs.readFileSync('./test-file.txt');
-  const s3FileUploadResponse = await s3Client.send(new PutObjectCommand({
-    Bucket: bucketName,
-    Key: 'test-file.txt',
-    Body: fileContent
-  }));
-  console.log("file uploaded:", s3FileUploadResponse)
+    // Upload file
+    const fileContent = fs.readFileSync('./test-file.txt');
+    const s3FileUploadResponse = await s3Client.send(new PutObjectCommand({
+        Bucket: bucketName,
+        Key: 'test-file.txt',
+        Body: fileContent
+    }));
+    console.log("file uploaded:", s3FileUploadResponse)
 
-  // List objects
-  const result = await s3Client.send(new ListObjectsCommand({ Bucket: bucketName }));
-  console.log('Uploaded files:', result.Contents);
+    // List objects
+    const result = await s3Client.send(new ListObjectsCommand({ Bucket: bucketName }));
+    console.log('Uploaded files:', result.Contents);
 }
 
 runS3Test().catch(console.error);
