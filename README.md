1fa2b0e098ace72896ec23bd76a6a71705d6ec19
ravvi-kumar <ravvi.kumar8989@gmail.com>
2025-02-14 14:53:11 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+To install dependencies:
+```sh
+bun install
+```
+
+To run:
+```sh
+bun run dev
+```
+
+open http://localhost:3000
+
+
+awslocal s3 ls s3://{BUCKET_NAME}
+awslocal s3 presign s3://{BUCKET_NAME}/{FILE_NAME}
---
+++ src/test.ts
@@ -0,0 +1,36 @@
+import { S3Client, CreateBucketCommand, PutObjectCommand, ListObjectsCommand } from "@aws-sdk/client-s3";
+import * as fs from 'fs';
+
+const s3Client = new S3Client({
+  endpoint: 'http://localhost:4566',
+//   endpoint: 'http://s3.localhost.localstack.cloud:4566',
+  forcePathStyle: true,
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: 'test',
+    secretAccessKey: 'test'
+  }
+});
+
+async function runS3Test() {
+  const bucketName = 'my-local-bucket';
+
+  // Create bucket
+  const bucketCreationResponse = await s3Client.send(new CreateBucketCommand({ Bucket: bucketName }));
+  console.log("bucket created:", bucketCreationResponse)
+
+  // Upload file
+  const fileContent = fs.readFileSync('./test-file.txt');
+  const s3FileUploadResponse = await s3Client.send(new PutObjectCommand({
+    Bucket: bucketName,
+    Key: 'test-file.txt',
+    Body: fileContent
+  }));
+  console.log("file uploaded:", s3FileUploadResponse)
+
+  // List objects
+  const result = await s3Client.send(new ListObjectsCommand({ Bucket: bucketName }));
+  console.log('Uploaded files:', result.Contents);
+}
+
+runS3Test().catch(console.error);
